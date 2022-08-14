f85c0074ceee4ebd3e5e23d4a9c849b927fc81e5
Trevor Vardeman <trevortx@gmail.com>
2022-08-13 18:58:14 -0700

    commit tree 7202799f44b727e2df567cf0028f2d120c8b4641

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    if title.present? && title.exclude?("Won't Believe" || "Secret" || "Top " || "Guess")
+      errors.add(:title, 'We need clicks, so we need more clickbait!')
+    end
+  end
 end
